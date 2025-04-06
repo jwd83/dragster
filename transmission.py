75c6a6ee595b8c909e0d86226390b422019f9ecc
Jared De Blander <jared0x90@gmail.com>
2025-04-06 10:33:31 -0400

    transmission updates

--- transmission.py
+++ transmission.py
@@ -9,13 +9,18 @@ class Transmission:
         self.max_gear = len(forward_gears)
 
     def output_rpm(self, input_rpm: float, gear: int) -> float:
-        pass
+        o_r = self.output_ratio(gear)
+        return input_rpm * o_r
 
     def output_ratio(self, gear: int) -> float:
-        pass
+        ir = self.input_ratio(gear)
+        if ir == 0.0:
+            return 0.0
+        return 1.0 / ir
 
     def input_rpm(self, output_rpm: float, gear: int) -> float:
-        pass
+        ir = self.input_ratio(gear)
+        return output_rpm * ir
 
     def input_ratio(self, gear: int) -> float:
         return self.final_drive * self.gear_ratio(gear)
@@ -32,3 +37,33 @@ class Transmission:
             raise ValueError(
                 f"Invalid gear: {gear}: int. Must be between -1 and {self.max_gear}."
             )
+
+    def gear_name(self, gear: int) -> str:
+        if gear == -1:
+            return "R"
+        elif gear == 0:
+            return "N"
+        elif 1 <= gear <= self.max_gear:
+            return f"{gear}"
+        else:
+            raise ValueError(
+                f"Invalid gear: {gear}: int. Must be between -1 and {self.max_gear}."
+            )
+
+
+if __name__ == "__main__":
+    # Example usage
+    t = Transmission([3.587, 2.022, 1.384, 1.0, 0.861], 4.0, 4.3)
+    for i in range(-1, 6):
+        orpm = t.output_rpm(1000, i)
+        print(f"Gear {t.gear_name(i)}: {t.gear_ratio(i):.3f}")
+        print(f"Input Ratio {t.gear_name(i)}: {t.input_ratio(i):.3f}")
+        print(f"Output Ratio {t.gear_name(i)}: {t.output_ratio(i):.3f}")
+        print(f"Output RPM @ 1000 input {t.gear_name(i)}: {orpm:.3f}")
+        print(
+            f"Input RPM @ output RPM {orpm:.3f} {t.gear_name(i)}: {t.input_rpm(orpm, i):.3f}"
+        )
+
+        print("")
+
+    print("Final Drive Ratio:", t.final_drive)
