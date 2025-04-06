e84ab1014b96a4efe416d8c7fdf803f2e5dd29c5
Jared De Blander <jared0x90@gmail.com>
2025-04-06 12:36:59 -0400

    starting simulation readout

--- vehicle.py
+++ vehicle.py
@@ -11,12 +11,27 @@ class Vehicle:
         self.weight = 2000
 
         self.current_gear = 1
-        self.current_speed = 0.0
+        self.current_speed_mph = 0.0
         self.current_engine_rpm = self.engine.min_rpm
+        self.current_throttle = 0.0
+
+    def readout(self) -> str:
+        return f"Gear: {self.current_gear}, Speed: {self.current_speed_mph:.2f} mph, RPM: {self.current_engine_rpm:.2f}"
+
+    def update(self):
+        tick: float = 1 / 60
+
+        # if the vehicle is not moving, accelerate it to the idle speed instantly to get moving
+        if self.current_speed_mph == 0:
+            if self.current_throttle > 0:
+                self.current_speed_mph = self.speed_mph_from_engine_rpm_and_gear()
+        else:
+            # if the vehicle is currently moving calculate the acceleration
+            self.current_speed_mph += tick
 
     def engine_rpm_from_speed_and_gear(self) -> float:
         # Calculate the engine RPM based on the current speed and gear
-        wheel_rpm = self.wheel.rpm_from_speed(self.current_speed)
+        wheel_rpm = self.wheel.rpm_from_speed(self.current_speed_mph)
         return self.transmission.input_rpm(wheel_rpm, self.current_gear)
 
     def speed_mph_from_engine_rpm_and_gear(self) -> float:
@@ -32,7 +47,7 @@ if __name__ == "__main__":
     vehicle = Vehicle()
     print("Vehicle initialized.")
 
-    print("Current speed:", vehicle.current_speed, "mph")
+    print("Current speed:", vehicle.current_speed_mph, "mph")
     print("Current gear:", vehicle.current_gear)
     print("Current engine RPM:", vehicle.current_engine_rpm)
 
@@ -40,3 +55,16 @@ if __name__ == "__main__":
     print(
         "Speed from engine RPM and gear:", vehicle.speed_mph_from_engine_rpm_and_gear()
     )
+
+    # starting sim
+    print("-" * 80)
+    print("Starting simulation...")
+    print("-" * 80)
+    vehicle.update()
+    print(vehicle.readout())
+
+    vehicle.current_throttle = 1.0  # Full throttle
+
+    for i in range(10):
+        vehicle.update()
+        print(vehicle.readout())
