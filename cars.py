d85d4b8aeb97145a983408697a02216e2033366d
Jared De Blander <jared@ayrscott.com>
2025-04-15 18:06:50 -0400

    rally car updates

--- cars.py
+++ cars.py
@@ -14,17 +14,18 @@ def build_rally() -> Vehicle:
     return Vehicle(
         Engine(
             [
-                (1000, 80),  # 1000 rpm, 80 ft-lb
-                (2000, 120),  # 2000 rpm, 120 ft-lb
-                (3000, 118),  # 3000 rpm, 150 ft-lb  turbo lag
-                (3800, 150),  # 3800 rpm, 150 ft-lb spool begins
-                (4100, 350),  # 4100 rpm, 350 ft-lb full boost
-                (6600, 330),  # 6600 rpm, 330 ft-lb turbo fade
-                (6800, 200),  # 6800 rpm, 200 ft-lb, turbo fade
+                (1000, 80),
+                (2000, 120),
+                (3000, 240),
+                (3800, 370),
+                (4100, 350),
+                (6600, 330),
+                (6800, 310),
                 (6950, 11),
-                (7000, 0),  # 7000 rpm fuel cutoff
+                (7000, 0),
             ],
-            shift_rpm=6820,
+            shift_rpm=6800,
+            launch_rpm=2400,
         )
     )
 
